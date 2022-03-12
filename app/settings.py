39a90548ef7ff5ceda78deb490a1f602e16a24b8
unknown <shubham0revadekar@gmail.com>
2022-03-13 02:04:17 +0530

    CORS support

--- app/settings.py
+++ app/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'logics',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 from pymongo import MongoClient
 
 #mongoClient = MongoClient('mongodb+srv://mongo:mongo@cluster0.9nefj.mongodb.net/scm?retryWrites=true&w=majority')
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
